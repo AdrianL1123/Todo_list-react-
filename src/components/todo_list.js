e69ca0dbd170547efcc1f7ec94415c1efde8fb85
[AdrianL] <[adrianlee061123@gmail.com]>
2024-03-18 11:34:16 +0800

    message

--- src/components/todo_list.js
+++ src/components/todo_list.js
@@ -2,10 +2,23 @@ import Todo_item from "./todo_item";
 import Add_new_form from "./add_new_form";
 export default function Todo_list() {
   const todos = [
-    { id: 1, text: "Task 1" },
-    { id: 2, text: "Task 2" },
-    { id: 3, text: "Task 3" },
+    {
+      id: 1,
+      text: "Task 1",
+      isCompleted: true,
+    },
+    {
+      id: 2,
+      text: "Task 2",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Task 3",
+      isCompleted: false,
+    },
   ];
+
   return (
     <div
       className="card rounded shadow-sm"
