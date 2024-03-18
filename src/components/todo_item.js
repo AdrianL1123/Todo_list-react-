f2f5aa4eefad1b6cf771b6df89b950e1c6987ab4
[AdrianL] <[adrianlee061123@gmail.com]>
2024-03-18 11:40:35 +0800

    message

--- src/components/todo_item.js
+++ src/components/todo_item.js
@@ -1,14 +1,12 @@
 export default function Todo_item(props) {
-  const { text, num } = props;
+  const { text } = props;
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
       <div>
         <button class="btn btn-sm btn-light">
           <i className="bi bi-square"></i>
         </button>
-        <span className="ms-2">
-          {text} {num}
-        </span>
+        <span className="ms-2">{text}</span>
       </div>
       <div>
         <button className="btn btn-sm btn-danger">
